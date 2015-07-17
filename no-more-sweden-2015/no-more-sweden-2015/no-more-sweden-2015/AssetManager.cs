1105734ce5afa92fab275a980f6c3c594339e91b
Tom Leonardsson <tom.leo@hotmail.com>
2015-07-17 15:39:40 +0200

    Gameobject manager

--- no-more-sweden-2015/no-more-sweden-2015/no-more-sweden-2015/AssetManager.cs
+++ no-more-sweden-2015/no-more-sweden-2015/no-more-sweden-2015/AssetManager.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
 
 namespace no_more_sweden_2015
 {
     class AssetManager
     {
+        public static Texture2D genericProjectile;
+
+        public void Load(ContentManager content)
+        {
+            genericProjectile = content.Load<Texture2D>("genericProjectile");
+        }
     }
 }
