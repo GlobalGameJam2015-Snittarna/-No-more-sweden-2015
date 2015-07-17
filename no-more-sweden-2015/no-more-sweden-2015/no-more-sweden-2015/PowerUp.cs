825b14c1ac99f3971b232993a879dfc38e6f17ef
Tom Leonardsson <tom.leo@hotmail.com>
2015-07-17 22:05:24 +0200

    Power ups

---
+++ no-more-sweden-2015/no-more-sweden-2015/no-more-sweden-2015/PowerUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace no_more_sweden_2015
+{
+    class PowerUp : GameObject
+    {
+        byte type;
+
+        public PowerUp(Vector2 position2, byte type2)
+        {
+            Position = position2;
+
+            type = type2;
+
+            Color = Color.White;
+            Scale = 1;
+
+            Speed = 3;
+
+            Angle = 90;
+        }
+
+        public override void Update()
+        {
+            Position += Velocity * Speed;
+
+            foreach (Player p in GameObjectManager.gameObjects.Where(item => item is Player))
+            {
+                if (HitBox.Intersects(p.HitBox))
+                {
+                    if (type <= 4)
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+
+            base.Update();
+        }
+
+        public override void DrawSprite(SpriteBatch spriteBatch)
+        {
+            base.DrawSprite(spriteBatch);
+        }
+    }
+}
