14852533f8619cbdde6fef5955139884a170c7f4
Tom Leonardsson <tom.leo@hotmail.com>
2015-07-18 13:21:09 +0200

    Adde background class

---
+++ no-more-sweden-2015/no-more-sweden-2015/no-more-sweden-2015/BackgroundObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace no_more_sweden_2015
+{
+    class BackgroundObject : GameObject
+    {
+        public BackgroundObject(Vector2 position2, byte frame, Random random2)
+        {
+            Position = position2;
+
+            Rotation = random2.Next(360);
+
+            Color = Color.White;
+            Frame = new Rectangle(frame * 128 + frame, 0, 128, 128);
+        }
+    }
+}
